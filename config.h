a9aca04244966db20e2ccf8d41733799078e126b
kev <kev@kev.com>
2024-06-07 20:29:57 +1000

    added config.h

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* macro for conveniently specifying pathu and pathc below */
-#define PATH(name)                      "/home/kev/.local/bin/dwmblocks/blocks/"name
+#define PATH(name)                      "$HOME/.local/bin/dwmblocks/blocks/"name
 
 /* buffer size for capturing output of the programs used for updating blocks */
 #define CMDOUTLENGTH                    50
