f704dcf4e3f66e2e987f91e41c7f81d6e9ab0823
kev <kev@kev.com>
2024-06-07 20:16:55 +1000

    calender update

--- blocks.def/calendar.sh
+++ blocks.def/calendar.sh
@@ -2,8 +2,8 @@
 ICON="📅"
 
 case $1 in
-	1) setsid -f "$TERMINAL" -e "cal -y; read -p 'Hit <enter> to exit' X;" ;;
-#	2) setsid -f "$TERMINAL" -e calcurse ;;
+	1) setsid -f "$TERMINAL" -e "cal -y; read 'X? Hit <enter> to exit' ;" ;;
+#	2) setsid -f "$TERMINAL" -e calcurse ;
 	3) notify-send "📅 Time/date module" "\- Left click to show calendar for the year" ;;
 	6) "$TERMINAL" -e "${EDITOR} $0" ;;
 esac
